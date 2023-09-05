5c7bb674cf7ef85b10395ff8e1a5651cf738ae6a
Md Raquib <70raquib@gmail.com>
2023-09-05 23:11:15 +0530

    feat - Footer is complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,14 @@
 // import React from 'react'
 
+import Logo from '../assets/images/Logo-1.png'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='mt-[80px] bg-secondary-light-color'>
+      <div className='flex flex-col gap-[40px] items-center justify-center px-10 pt-6'>
+        <img src={Logo} alt="logo" className='w-[200px] h-[40px] mb-8'/>
+      </div>
+    </div>
   )
 }
 
