113d0369140e209874bb2e779e176d4481c81ccd
Raquib <70raquib@gmail.com>
2023-09-12 23:51:53 +0530

    style - changed the logos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@
 const Footer = () => {
   return (
     <div className='mt-[80px] bg-secondary-light-color'>
-      <div className='flex flex-col gap-[40px] items-center justify-center px-10 pt-6'>
-        <img src='../assets/images/Logo-1.png' alt="logo" className='w-[200px] h-[40px] mb-8'/>
+      <div className='flex flex-col gap-[40px] items-center justify-center px-10 py-2'>
+        <img src='../assets/images/Logo-1.png' alt="logo" className='w-[180px] h-[70px]'/>
       </div>
     </div>
   )
